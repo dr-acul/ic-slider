c463dfcb5f5ea67f37f3801ba944649f68b448b8
dr-acul <dr.acul@posteo.de>
2016-04-22 15:38:07 +0200

    - rewrite standalone slider to plugin - some small changes

--- ic-slider.js
+++ ic-slider.js
@@ -1,23 +1,15 @@
 // move forward (images slide in left direction);
 function moveForward(current, next) {
 	next.css("marginLeft", "100%");
-	current.queue( "fx" );
-
-// dosen't really change much when spam clicking, maybe this is the standard behavior anyway.
-	current.queue( function() {
-		jQuery( current.animate( {marginLeft: "-100%"}, "slow") ).dequeue();
-	} );
-	
-	next.queue( function() {
-		jQuery( next.animate( {marginLeft: "0"}, "slow") ).dequeue();
-	});
+	current.animate({marginLeft: "-100%"}, "fast");
+	next.animate({marginLeft: "0"}, "fast");
 }
 
 //move backward (images slide in right direction);
 function moveBackward(current, next) {
 	next.css("marginLeft", "-100%");
-	current.animate( {marginLeft: "100%"}, "slow");
-	next.animate( {marginLeft: "0"}, "slow");
+	current.animate({marginLeft: "100%"}, "slow");
+	next.animate({marginLeft: "0"}, "slow");
 }
 
 function toggleActiveClasses(current, next) {
@@ -25,79 +17,92 @@ function toggleActiveClasses(current, next) {
 	next.toggleClass("active");
 }
 
-function animateFactory( i ) {
-	return function() {
-		var activeImage = jQuery( "div.ic_slider_imgdiv.active" );
-		var activeButton = jQuery( "button.ic_slider_dots.active" );
-		var nextImage = jQuery( "div#ic_slider_img_" + i );
-		var nextButton = jQuery( "[name=ic_slider_" + i + "]" );
+function animateFactory(i) {
+	return function () {
+		var activeImage = jQuery(".ic_slider_animation_container.active");
+		var activeButton = jQuery("button.ic_slider_dots.active");
+		var nextImage = jQuery(".ic_slider_animation_container:eq(" + i + ")");
+		var nextButton = jQuery("[name=ic_slider_" + i + "]");
 		// don't move when the new images is the old one
-		if ( !jQuery( activeImage ).is( nextImage ) ) {
+		if (!jQuery(activeImage).is(nextImage)) {
 			// do we move forward ?
-			if ( jQuery( activeImage ).nextAll().is( nextImage ) ) {
+			if (jQuery(activeImage).nextAll().is(nextImage)) {
 				moveForward(activeImage, nextImage);
 			} else { // we move backward !
 				moveBackward(activeImage, nextImage);
 			}
-			
+
 			toggleActiveClasses(activeImage, nextImage);
 			toggleActiveClasses(activeButton, nextButton);
 		}
 	};
 }
 
-jQuery( document ).ready(function() {	
+jQuery(document).ready(function () {
 	// set click functions for buttons.
 	for (i = 0; i < 4; i++) {
-		jQuery( "[name=ic_slider_" + i + "]" ).click( animateFactory( i ) );
-	};
-	
+		jQuery("[name=ic_slider_" + i + "]").click(animateFactory(i));
+	}
+	;
+
 	// setup left/right menu fade	
-	jQuery( "#ic_slider_wrapper" ).hover( function() {
-		jQuery( "#ic_slider_left_nav" ).fadeIn();
-		jQuery( "#ic_slider_right_nav" ).fadeIn();
-	}, function() {
-		jQuery( "#ic_slider_left_nav" ).fadeOut();
-		jQuery( "#ic_slider_right_nav" ).fadeOut();
-	} );
-	
+	jQuery("#ic_slider_wrapper").hover(function () {
+		jQuery("#ic_slider_left_nav").fadeIn();
+		jQuery("#ic_slider_right_nav").fadeIn();
+	}, function () {
+		jQuery("#ic_slider_left_nav").fadeOut();
+		jQuery("#ic_slider_right_nav").fadeOut();
+	});
+
 	// fade out left/right menu at startup
-	jQuery( "#ic_slider_left_nav" ).fadeOut();
-	jQuery( "#ic_slider_right_nav" ).fadeOut();
-	
-	jQuery( "#ic_slider_left_nav" ).click( function() {
-		var activeImage = jQuery( "div.ic_slider_imgdiv.active" );
-		var activeButton = jQuery( "button.ic_slider_dots.active" );
+	jQuery("#ic_slider_left_nav").fadeOut();
+	jQuery("#ic_slider_right_nav").fadeOut();
+
+/* TODO: remove image bound slider */
+	jQuery("#ic_slider_left_nav").click(function () {
+		var activeImage = jQuery(".ic_slider_animation_container.active");
+		var activeButton = jQuery("button.ic_slider_dots.active");
+		
 		// Do we hit the endge ?
-		if ( activeImage.prev().length > 0 ) {
-			moveBackward(activeImage, activeImage.prev() );
-			toggleActiveClasses(activeImage, activeImage.prev() );
-			toggleActiveClasses(activeButton, activeButton.prev() );
+		if (activeImage.prev().length > 0) {
+			moveBackward(activeImage, activeImage.prev());
+			toggleActiveClasses(activeImage, activeImage.prev());
+			toggleActiveClasses(activeButton, activeButton.prev());
 		} else {
 			//this will select the last (next) element in the chain
-			var nextImage = jQuery( "div.ic_slider_imgdiv" ).last();
-			var nextButton = jQuery( "button.ic_slider_dots" ).last();
-			moveBackward( activeImage, nextImage );
-			toggleActiveClasses(activeImage, nextImage );
-			toggleActiveClasses(activeButton, nextButton );
+			var nextImage = jQuery(".ic_slider_animation_container").last();
+			var nextButton = jQuery("button.ic_slider_dots").last();
+			moveBackward(activeImage, nextImage);
+			toggleActiveClasses(activeImage, nextImage);
+			toggleActiveClasses(activeButton, nextButton);
 		}
-	} );
-	
-	jQuery( "#ic_slider_right_nav" ).click( function() {
-		var activeImage = jQuery( "div.ic_slider_imgdiv.active" );
-		var activeButton = jQuery( "button.ic_slider_dots.active" );
+	});
+
+	jQuery("#ic_slider_right_nav").click(function () {
+		var activeImage = jQuery(".ic_slider_animation_container.active");
+		var activeButton = jQuery("button.ic_slider_dots.active");
+		
+		var activeContainer = jQuery(".ic_slider_animation_container.active");
 		// Do we hit the endge ?
-		if ( activeImage.next().length > 0 ) {
-			moveForward(activeImage, activeImage.next() );
-			toggleActiveClasses(activeImage, activeImage.next() );
-			toggleActiveClasses(activeButton, activeButton.next() );
+		if (activeImage.next().length > 0) {
+			moveForward(activeImage , activeImage.next());
+			toggleActiveClasses(activeImage, activeImage.next());
+			toggleActiveClasses(activeButton, activeButton.next());
 		} else {
 			//this will select the first (next) element in the chain
-			var nextImage = jQuery( "div.ic_slider_imgdiv" ).first();
-			var nextButton = jQuery( "button.ic_slider_dots" ).first();
-			moveForward( activeImage, nextImage );
-			toggleActiveClasses(activeImage, nextImage );
-			toggleActiveClasses(activeButton, nextButton );
+			var nextImage = jQuery(".ic_slider_animation_container").first();
+			var nextButton = jQuery("button.ic_slider_dots").first();
+			moveForward(activeImage, nextImage);
+			toggleActiveClasses(activeImage, nextImage);
+			toggleActiveClasses(activeButton, nextButton);
 		}
-	} );
+	});
 });
+
+function hideEffect(effect_container, direction) {
+	
+}
+
+function showEffect(effect_container, direction) {
+
+}
--- wp-ic-slider.php
+++ wp-ic-slider.php
@@ -1,65 +1,69 @@
 <?php
 /*
-Plugin Name: IN|creare - Slider
-Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
-Description: A collection of settings for the IN|creare packages
-Version: 0.01
-Author: the IN|creare team
-Author URI: http://www.increare.de
-License: A "Slug" license name e.g. GPL2
-*/
+  Plugin Name: IN|creare - Slider
+  Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+  Description: A collection of settings for the IN|creare packages
+  Version: 0.01
+  Author: the IN|creare team
+  Author URI: http://www.increare.de
+  License: A "Slug" license name e.g. GPL2
+ */
 ?>
 
 <?php
+
 function ic_slider_register_styles() {
-    wp_register_style( 'ic-slider-stylesheet', plugins_url( 'ic-slider.css', __FILE__ ) );
-	wp_enqueue_style( 'ic-slider-stylesheet' );
+	wp_register_style('ic-slider-stylesheet', plugins_url('ic-slider.css', __FILE__));
+	wp_enqueue_style('ic-slider-stylesheet');
 }
-add_action( 'wp_print_styles', 'ic_slider_register_styles' );
+
+add_action('wp_print_styles', 'ic_slider_register_styles');
 
 function ic_slider_enqueue_js() {
-	wp_enqueue_script(	'ic-slider-js', plugin_dir_url( __FILE__ ) . 'ic-slider.js',
-						array( 'jquery' ) );
+	wp_enqueue_script('ic-slider-js', plugin_dir_url(__FILE__) . 'ic-slider.js', array('jquery', 'jquery-effects-shake'));
 }
-add_action( 'wp_print_scripts', 'ic_slider_enqueue_js' );
 
+add_action('wp_print_scripts', 'ic_slider_enqueue_js');
 ?>
 
-<?php function start_ic_slider() { ?>
-<noscript>
+<?php
+
+function start_ic_slider() { ?>
+	<noscript>
 	<div><p>Please Enable JavaScript to view this Site.</p></div>
-</noscript>
-<div id="ic_slider_wrapper">
-	<div id="ic_slider_img_wrapper">
-		<div id="ic_slider_img_position_wrapper">
-			<div id="ic_slider_img_0" class="ic_slider_imgdiv active">
-	 			  <img src="<?php echo plugins_url( 'images/nature_mountain.png', __FILE__ ); ?>" />
+	</noscript>
+	<div id="ic_slider_wrapper">
+		<div id="ic_slider_position_wrapper">
+			<div class="ic_slider_animation_container active">
+				<img src="<?php echo plugins_url('images/nature_mountain.png', __FILE__); ?>" />
+<!--
+				<div id="ic_slider_text_wrapper">
+					<h1>the_title()</h1>
+					<p>the_content()</p>
+				</div><!-- #ic_slider_text_wrapper -->
 			</div>
-			<div id="ic_slider_img_1" class="ic_slider_imgdiv">
-	 			<img src="<?php echo plugins_url( 'images/nature_panorama.png', __FILE__ ); ?>" />
+			<div class="ic_slider_animation_container">
+				<img src="<?php echo plugins_url('images/nature_panorama.png', __FILE__); ?>" />
 			</div>
-			<div id="ic_slider_img_2" class="ic_slider_imgdiv">
-	 			<img src="<?php echo plugins_url( 'images/panorama_mountains_peaks.png', __FILE__ ); ?>" />
+			<div class="ic_slider_animation_container">
+				<img src="<?php echo plugins_url('images/panorama_mountains_peaks.png', __FILE__); ?>" />
 			</div>
-			<div id="ic_slider_img_3" class="ic_slider_imgdiv">
-	 			<img src="<?php echo plugins_url( 'images/wonderfully_wild_faroe_isl.png', __FILE__ ); ?>" />
+			<div class="ic_slider_animation_container">
+				<img src="<?php echo plugins_url('images/wonderfully_wild_faroe_isl.png', __FILE__); ?>" />
 			</div>
 		</div><!-- #ic_slider_img_position_wrapper -->
-	</div><!-- ic_slider_img_wrapper -->
-
-	<div id="ic_slider_left_nav">
-			<img src="<?php echo plugins_url( 'images/arrow_left.png', __FILE__ ); ?>" />
-	</div>
-	<div id="ic_slider_right_nav">
-		<img src="<?php echo plugins_url( 'images/arrow_right.png', __FILE__ ); ?>" />
-	</div>	
-	<div id="ic_slider_dots_wrapper">
-		<button class="ic_slider_dots active" name="ic_slider_0"></button>
-		<button class="ic_slider_dots" name="ic_slider_1"></button>
-		<button class="ic_slider_dots" name="ic_slider_2"></button>
-		<button class="ic_slider_dots" name="ic_slider_3"></button>
-	</div><!-- #ic_slider_dots_wrapper -->
-</div><!-- #ic_slider_wrapper -->
-<?php
+		<div id="ic_slider_left_nav">
+			<img src="<?php echo plugins_url('images/arrow_left.png', __FILE__); ?>" />
+		</div>
+		<div id="ic_slider_right_nav">
+			<img src="<?php echo plugins_url('images/arrow_right.png', __FILE__); ?>" />
+		</div>	
+		<div id="ic_slider_dots_wrapper">
+			<button class="ic_slider_dots active" name="ic_slider_0"></button>
+			<button class="ic_slider_dots" name="ic_slider_1"></button>
+			<button class="ic_slider_dots" name="ic_slider_2"></button>
+			<button class="ic_slider_dots" name="ic_slider_3"></button>
+		</div><!-- #ic_slider_dots_wrapper -->
+	</div><!-- #ic_slider_wrapper -->
+	<?php
 }
-?>
